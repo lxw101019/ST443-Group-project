816f4de4bd8fa3819ea1eeaf659fe61084f6b2f6
Supernova.L <x.lin28@lse.ac.uk>
2019-12-02 13:01:38 +0000

    update

--- 2.1-Node-wise-lasso/Choose-best-lambda.R
+++ 2.1-Node-wise-lasso/Choose-best-lambda.R
@@ -7,15 +7,16 @@
 # Retutn: 1.final: which is the best lambda,in each case of making X_i as response, there will be a lambda that minimised the test error.
 #                  In this case, the best lambda 'final' is the mean of these lambda. Further discussion needed here.
 choose_best_lambda <- function(data){
+  library(glmnet)
   numOfRows <- nrow(data)
   numOfDims <- ncol(data)
   
   lambda.best_list = rep(0,numOfDims)
   for (i in seq(numOfDims)){
-    y <-(testdata[,i])
+    y <- (testdata[,i])
     x <- (testdata[,-c(i)])
     
-    train <-sample(seq(numOfRows), 0.6*numOfRows, replace=FALSE)
+    train <-sample(seq(numOfRows), 0.7*numOfRows, replace=FALSE)
     lasso.train <-glmnet(x[train,], y[train])
     pred.test <-predict(lasso.train, x[-train,])
     rmse <-sqrt(apply((y[-train]-pred.test)^2,2,mean))
