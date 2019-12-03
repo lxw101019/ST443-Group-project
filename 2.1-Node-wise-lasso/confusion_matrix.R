7b29ca72b88e1b6ebf301e27c5cccdde5356f23b
Supernova.L <x.lin28@lse.ac.uk>
2019-12-03 00:09:46 +0000

    inculding test_file and fix a bug in confusion_matrix

--- 2.1-Node-wise-lasso/confusion_matrix.R
+++ 2.1-Node-wise-lasso/confusion_matrix.R
@@ -1,19 +1,19 @@
 # This is the code for generating confusion matrix/ROC curve point based on given edtimated edge table and true edge table.
 # 
 # Arguments:1. estimate_edge: the TRUE/FALSE table from edgetable.R
-#           2. true_edge: the TRUE/FALSE table from True_edge.R
+#           2. trueEdge: the TRUE/FALSE table from trueEdge.R
 #           3. estimate_way: input 'both' or 'either' here. 'both' stands for E_1 estimator. 'either' stands for E_2 estimator.
 #
 # Retutn: 1.confusion: a list. Use list$TP/FP/FN/TN/TP_rate/FP_rate to get each confusion element or ROC curve point
 #
-confusion_matrix <- function(estimate_edge, true_edge, estimate_way){
+confusion_matrix <- function(estimate_edge, trueEdge, estimate_way){
   numOfDims <- nrow(estimate_edge)
   confusion <- list(TP = 0,FP = 0,FN = 0,TN = 0,TP_rate = 0,FP_rate = 0)
   
-  for (i in seq(numOfDims)){
+  for (i in seq(numOfDims-1)){
     for (j in seq(numOfDims)[seq(numOfDims)>i]){
       if (estimate_way == 'either'){
-        if (true_edge[i,j] == TRUE){
+        if (trueEdge[i,j] == TRUE){
           if (estimate_edge[i,j] == FALSE & estimate_edge[j,i] == FALSE){
             confusion$FN <- confusion$FN + 1
           } else {
@@ -27,7 +27,7 @@ confusion_matrix <- function(estimate_edge, true_edge, estimate_way){
           }
         }
       } else if (estimate_way == 'both') {
-        if (true_edge[i,j] == TRUE){
+        if (trueEdge[i,j] == TRUE){
           if (estimate_edge[i,j] == TRUE & estimate_edge[j,i] == TRUE){
             confusion$TP <- confusion$TP + 1
           } else {
