a5683e141b3fee0550863d013f52ac26daa45e55
Supernova.L <x.lin28@lse.ac.uk>
2019-12-04 01:43:12 +0000

    description included

--- 2.1-Node-wise-lasso/ROC_curve.R
+++ 2.1-Node-wise-lasso/ROC_curve.R
@@ -1,8 +1,23 @@
-library(ggplot2)
-ROC_curve <- function(data_set, theta, estimate_way, how_many_lambda_in_roc = 1000){
+# This is the code for preparing the points that should be plotted on a ROC curve.
+# Since we know for different lambda there will be different lasso coef->different estimated table->different confusion matrix->different FPR TPR points
+# This function will first calculate the lambda range then use evenly spaced lambdas to generate points(TPR and FPR).
+# This function is the most time-consuming since it call all the other function I wrote and loop them over and over.
+# 
+# Arguments:1. data_set: our simulated data set.
+#           2. theta: the standard theta/general theta for simulation. we use it to run true_edge/confusion_matrix functions.
+#           3. estimate_way: input 'both' or 'either' here. 'both' stands for E_1 estimator. 'either' stands for E_2 estimator.
+#           4. how_many_lambda_in_roc : default is 200 This instance how many points there will be shown on the ROC Curve. Since the ROC curve was plotted by (FPR, TPR) for different lambda,
+#              so after we calculate the range of lambda, we evenly space the range into 'how_many_lambda_in_roc' pieces. 
+# 
+# Retutn: 1.points:a (how_many_lambda_in_roc,3) sized data frame. Storing three data: FPR,TPR,Lambda as columns.
+#
+# Usage: after generating ROC Points, we should manually use ggplots to plot these point, sample code are provided after the function.
+########################################################################################################################################################################################################
+
+ROC_curve <- function(data_set, theta, estimate_way, how_many_lambda_in_roc = 200){
 
   numOfDims <- ncol(data_set)
-  print(numOfDims)
+  
   max_lambda = 1
   min_lambda = 0
   points <- data.frame(matrix(ncol = 3, nrow = how_many_lambda_in_roc))
@@ -24,7 +39,9 @@ ROC_curve <- function(data_set, theta, estimate_way, how_many_lambda_in_roc = 10
   #print(lambda_seq)
   points[,3] <- lambda_seq
   true_edge <- true_edge(theta)
+  
   for (i in seq(length(lambda_seq))){
+    print(c('Calculating points:',i))
     estimate_edge <- edge_table(data_set, lambda_seq[i])
     #print(estimate_edge)
     confusion <- confusion_matrix(estimate_edge, true_edge, estimate_way)
@@ -37,12 +54,10 @@ ROC_curve <- function(data_set, theta, estimate_way, how_many_lambda_in_roc = 10
     points[i,2] <- FPR
   }
   colnames(points) <- c("TPR","FPR","Lambda")
-  
   return(points)
 }
+
+#after generating ROC Points, we should manually use ggplots to plot these point
+library(ggplot2)
 ggplot(testtest, aes(FPR, TPR)) + geom_step()
 
-#testdata <- simulation(100,1000)
-#data <- testdata$data
-#theta <- testdata$standardtheta
-#testtest <- ROC_curve(data,theta,"either")
--- 2.1-Node-wise-lasso/test_file.R
+++ 2.1-Node-wise-lasso/test_file.R
@@ -1,4 +1,4 @@
-testsample <- simulation(500,1000)
+testsample <- simulation(100,20)
 testdata <- testsample$data
 testtheta <-testsample$standardtheta
 lambda_MINTESTERROR <- choose_best_lambda(testdata)
@@ -8,7 +8,7 @@ estimate_edge <- edge_table(testdata, lambda_MINTESTERROR)
 table <- confusion_matrix(estimate_edge, trueedge, "either")
 table
 
-roc <- ROC_curve(testdata, testtheta,"either",100)
+roc <- ROC_curve(testdata, testtheta,"either",1000)
 
 library(ggplot2)
 ggplot(roc, aes(FPR, TPR)) + geom_step() + geom_point()
