b46c85f8578fa7ad5ea0fc34f444eb58640eb02d
superlaut <laurens@vdmaas.nl>
2019-12-08 11:31:43 +0000

    Some changes to 2.2

--- 2.2-Graphical-lasso/make_roc_glasso.R
+++ 2.2-Graphical-lasso/make_roc_glasso.R
@@ -13,18 +13,17 @@ testtheta <-testsample$standardtheta
 trueedge <- true_edge(testtheta)
 
 s <- cov(testdata)
-glassotry <- glasso(s, rho = 0.1)
 
-estimate_edge <- true_edge(glassotry$wi)
-#confusion matrix based on the estimated edge and "real" edge we just generated.
-
-#This function will return a list which include not just the confusion matrix but also TPR and FPR
-table <- confusion_matrix(estimate_edge, trueedge)
-table
 
 #roc curve: this function use different lambdas(calculated inside) to calculate each TPR and FPR, and return a "point matrix", will need ggplot to plot it later
-roc <- ROC_curve(testdata, testtheta, lambdamin = 0, lambdamax = 0.5, 200)
+roc <- ROC_curve(testdata, testtheta, lambdamin = 0, lambdamax = 0.2, 200)
 
 # based on the result given by the ROC_curve , we plot the ROC curve here.
-ggplot(roc, aes(FPR, TPR)) + geom_step() + xlim(0,1) + ylim(0,1) 
+par(mfrow=c(2,2))
+ggplot(roc, aes(FPR, TPR)) + geom_step() + xlim(0,1) + ylim(0,1)  + geom_point(alpha = 0.3)
+
 DescTools::AUC(x = roc$FPR,y = roc$TPR)
+
+# p 5, 20, 50, 100
+# n  10000
+# calculate AUC
--- 2.2-Graphical-lasso/select_tuning_param.R
+++ 2.2-Graphical-lasso/select_tuning_param.R
@@ -1,45 +1,45 @@
 library(glasso)
 library(qgraph)
 
-set.seed(123)
-sampleData <- simulation(p = 20, n = 1000)
-trueTheta <- sampleData$standardtheta
 
-trainIndex <- sample(seq(nrow(sampleData$data)), floor(nrow(sampleData$data))/2, replace=FALSE)
-train <- sampleData$data[trainIndex,]
-test <- sampleData$data[-trainIndex,]
-sTrain <- cov(train)
-sTest <- cov(test)  # Empirical Covariance Matrix
-sAll <- cov(sampleData$data)
-
-ll <- data.frame(rho = vector(length=2001), logl = vector(length = 2001))
-count <- 0
-for(rhos in seq(0,0.2,0.0001)){
-  count <- count + 1
-  glassoOutput <- glasso(sTrain, rho = rhos)
-  modelPrecMat <- glassoOutput$wi
-  ll$logl[count] <- log(det(modelPrecMat)) - sum(diag(sTest %*% modelPrecMat))
-  ll$rho[count] <- rhos
+for(i in 1:50){
+  sampleData <- simulation(p = 20, n = 1000)
+  trueTheta <- sampleData$standardtheta
+  
+  trainIndex <- sample(seq(nrow(sampleData$data)), floor(nrow(sampleData$data))/2, replace=FALSE)
+  train <- sampleData$data[trainIndex,]
+  test <- sampleData$data[-trainIndex,]
+  sTrain <- cov(train)
+  sTest <- cov(test)  # Empirical Covariance Matrix
+  sAll <- cov(sampleData$data)
+  
+  ll <- data.frame(rho = vector(length=2001), logl = vector(length = 2001))
+  count <- 0
+  
+  for(rhos in seq(0,0.2,0.0001)){
+    count <- count + 1
+    glassoOutput <- glasso(sTrain, rho = rhos)
+    modelPrecMat <- glassoOutput$wi
+    ll$logl[count] <- log(det(modelPrecMat)) - sum(diag(sTest %*% modelPrecMat))
+    ll$rho[count] <- rhos
+  }
+  
+  index <- which.max(ll$logl)
+  glassoBest <- glasso(sAll, ll$rho[index])
+  
+  true <- true_edge(trueTheta)
+  modelfit <- true_edge(glassoBest$wi)
+  cm <- confusion_matrix(modelfit,true)
+  cm$TP_rate
+  cm$FP_rate
 }
+  
 
-index <- which.max(ll$logl)
-glassoGood <- glasso(sAll, ll$rho[index])
-
-true <- true_edge(trueTheta)
-modelfit <- true_edge(glassoGood$wi)
-confusion_matrix(modelfit,true)
-#0.0505
-
-
-ebic <- EBICglasso(s, n = 500, returnAllResults = TRUE, 
-                   nlambda = 1000, lambda.min.ratio = 0.001, gamma = 0.5, threshold = TRUE)
-optLambdaEBIC <- ebic$lambda[which.min(ebic$ebic)]
-optMatrixEBIC <- ebic$optnet
-
-EBICgraph <- qgraph(glassoGood$wi - diag(diag(glassoGood$wi)), layout = "spring", title = "EBIC")
+EBICgraph <- qgraph(glassoBest$wi - diag(diag(glassoBest$wi)), layout = "spring", title = "EBIC")
 
 # log det theta - trace(S theta)
 
-#glasso(s, rho, nobs=NULL, zero=NULL, thr=1.0e-4, maxit=1e4,  approx=FALSE, penalize.diagonal=TRUE, start=c("cold","warm"), w.init=NULL,wi.init=NULL, trace=FALSE)
+# p 5, 20, 50, 100
+# n 10, 100, 1000, 10000
 
 
