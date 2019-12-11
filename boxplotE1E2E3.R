fe842059990e55c29a0dec049e04c2dfce63d0e7
superlaut <laurens@vdmaas.nl>
2019-12-11 22:34:52 +0000

    yay

--- boxplotE1E2E3.R
+++ boxplotE1E2E3.R
@@ -1,11 +1,10 @@
 library(tidyverse)
 
-dfAll <- read.csv('E1E2E3.csv')
+dfAll <- read.csv('E1E2E3withMCE.csv')
 dfAll <- dfAll[,-1]
 dfAll$p <- as.factor(dfAll$p)
 dfAllTPR <- filter(dfAll, method == 'cv_min' | method == 'loglikhood')
 dfAllFPR <- filter(dfAll, method == 'cv1se' | method == 'loglikhood')
-
 
 dfAllTPRn100 <- filter(dfAllTPR, n == 100)
 dfAllTPRn1000 <- filter(dfAllTPR, n == 1000)
@@ -14,8 +13,6 @@ dfAllTPRn10000 <- filter(dfAllTPR, n == 10000)
 dfAllFPRn100 <- filter(dfAllFPR, n == 100)
 dfAllFPRn1000 <- filter(dfAllFPR, n == 1000)
 dfAllFPRn10000 <- filter(dfAllFPR, n == 10000)
-
-
 
 
 #TPR
@@ -41,6 +38,17 @@ pn10000FPR <- ggplot(dfAllFPRn10000, aes(x=Estimated_E, y=FPR)) +
   geom_boxplot(aes(fill = p)) +
   ggtitle("n = 10000") + ylim(0,0.5)
 
+#MCE 
+pn100MCE <- ggplot(dfAllFPRn100, aes(x=Estimated_E, y=MCE)) +
+  geom_boxplot(aes(fill = p)) +
+  ggtitle("n = 100") + ylim(0,0.5)
+pn1000MCE <- ggplot(dfAllFPRn1000, aes(x=Estimated_E, y=MCE)) +
+  geom_boxplot(aes(fill = p)) +
+  ggtitle("n = 1000") + ylim(0,0.5)
+pn10000MCE <- ggplot(dfAllFPRn10000, aes(x=Estimated_E, y=MCE)) +
+  geom_boxplot(aes(fill = p)) +
+  ggtitle("n = 10000") + ylim(0,0.5)
+
 library(gridExtra)
 grid.arrange(pn100TPR, pn1000TPR, pn10000TPR, ncol = 3)
 gTPR <- arrangeGrob(pn100TPR, pn1000TPR, pn10000TPR, ncol = 3) #generates g
@@ -49,3 +57,8 @@ ggsave(file="TPRbpALL.pdf", gTPR)
 grid.arrange(pn100FPR, pn1000FPR, pn10000FPR, ncol = 3)
 gFPR <- arrangeGrob(pn100FPR, pn1000FPR, pn10000FPR, ncol = 3) #generates g
 ggsave(file="FPRbpALL.pdf", gFPR)
+
+grid.arrange(pn100MCE, pn1000MCE, pn10000MCE, ncol = 3)
+gMCE <- arrangeGrob(pn100MCE, pn1000MCE, pn10000MCE, ncol = 3) #generates g
+ggsave(file="MCEbpALL.pdf", gMCE)
+
